39583262a3de965fd3cb0f6c99ac10142ea5db51
IcaroM-CdC <icaro.moreira@protonmail.com>
2023-06-20 14:56:05 -0300

    iniciando

--- client/src/pages/landingPage/landingPage.tsx
+++ client/src/pages/landingPage/landingPage.tsx
@@ -1,9 +1,37 @@
 import React from "react"
+import "./landingPage.css"
 
 export default function LandingPage(){
     return (
-        <div>
-            ABACATE
+        <div id="landing-page-main">
+            <div id="landing-page-body">
+                <div id="landing-page-body-about">
+
+                </div>
+                <div id="landing-page-body-login-register">
+                    <div id="landing-page-body-login-register-container">
+                        <div id="landing-page-body-login-register-container-logo">
+
+                        </div>
+                        <div id="landing-page-body-login-register-container-form">
+                            <div id="landing-page-body-login-form">
+                                <div id="landing-page-body-login-form-textfield">
+                                    <input id="username" className="login-textfields" type="text" />
+                                    <input id="password" className="login-textfields" type="text" />
+                                </div>
+                                <div id="landing-page-body-login-form-buttons">
+                                    <input id="login-button" className="login-login-buttons" type="button" />
+                                    <input id="register-button" className="login-login-buttons" type="button" />
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="landing-page-footer">
+
+            </div>
         </div>
     )
 }
